b64299cb60fb734c43d81f1be6b47fd674c06d33
Devon Quaternik <devon.quaternik@gmail.com>
2018-05-17 12:56:05 -0700

    Decipher doesn't work, need to learn to map the variable j correctly

--- decipher.py
+++ decipher.py
@@ -1,3 +1,4 @@
+#not working. Need to work out why j isn't working for my index and instead is giving me the same thing. 
 import string
 def decipher(word, cmsg):
     print(cmsg)
@@ -9,10 +10,11 @@ def decipher(word, cmsg):
         wstr+=wstr[len(wstr)%frstlen]
     oglist = list(string.ascii_lowercase)
     char = [[oglist[(x+y)%26] for x in range(26)] for y in range(26)]
-    
-    j = ord(wstr[count])-97
-    while len(msg) != len(cmsg):
+
+    while count != len(cmsg):
+        j = ord(wstr[count])-97
         for x in char[j]:
+            print(x)
             i = ord(x)-97
             if cmsg[count] == char[j][i]:
                 msg+=char[j][i]
